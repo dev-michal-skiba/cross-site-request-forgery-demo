eb9d8b0063e7af7b2583fa9069d54033cb45cd8f
michalskiba <michalskiba@scurri.com>
2023-01-16 20:54:41 +0100

    Added demo data and fixed account form

--- csrf_demo/account/forms.py
+++ csrf_demo/account/forms.py
@@ -9,8 +9,8 @@ class LoginForm(forms.Form):
 class RegisterForm(forms.Form):
     username = forms.CharField(label='Username', max_length=64)
     email = forms.EmailField(label='Email', max_length=64)
-    password = forms.CharField(label='Password', widget=forms.PasswordInput(), min_length=12, max_length=64)
+    password = forms.CharField(label='Password', widget=forms.PasswordInput(), min_length=8, max_length=64)
 
 
 class UpdateForm(forms.Form):
-    password = forms.CharField(label='Change password', widget=forms.PasswordInput(), min_length=12, max_length=64)
+    password = forms.CharField(label='Change password', widget=forms.PasswordInput(), min_length=8, max_length=64)
